e2b4f000c65a48e1830e926722115da5c085305d
Raphael <euraphaelbrandao@gmail.com>
2025-07-13 13:08:45 -0300

    Novo projeto conta banco.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
+        Integer Numero = 0 ;
+        String agencia = "";
+        String nomeCliente = "";
+        Double saldo = 237.48;
+
+        System.out.println("Por favor, digite o número da Agência");
+
+        ;
 
         }
+
     }
